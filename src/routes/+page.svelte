8e59be4cae1acaea01d6c3d5d90a0ae92a8a04b8
marendra <marendra79@yahoo.com>
2023-12-07 10:58:45 +0700

    commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import eei from "../asset/eei.png"
+</script>
+<div class="w-full h-screen mx-auto flex flex-col justify-center items-center bg-white">
+	<p class="text-4xl mb-8 text-black">Website Under Construction</p>
+	<img src={eei} alt="EEI">
+</div>
